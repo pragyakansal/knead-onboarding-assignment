2b2a16ef1ecc692791cae730076832ab30d9cbb8
Rashmika Ranjan <60105042+rashmikaranjan@users.noreply.github.com>
2025-07-25 15:51:48 -0400

    added comments

--- knead-backend/main.py
+++ knead-backend/main.py
@@ -9,6 +9,7 @@ from uuid import UUID, uuid4
 
 app = FastAPI()
 
+# allow CORS for all origins
 origins = [ "*" ]
 
 app.add_middleware(
@@ -19,19 +20,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class Comment(BaseModel):
-    message: str
+# can be changed as per requirements
+# class Comment(BaseModel):
+#     message: str
 
+# used pydantic model to define input structure for a strategy
 class Strategy(BaseModel):
     id: UUID | None = None
     title: str
     # Changed to comment for now, will update if needed after clarification
     comment: str | None = None
-    #description: str | None = None
-    #comments: List[Comment] | None = None
+    # description: str | None = None
+    # comments: List[Comment] | None = None
 
+# in-memory storage
 strategies = []
 
+# handle input validation failures
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
     return PlainTextResponse(str(exc), status_code=400)
@@ -42,8 +47,9 @@ def getStrategies():
 
 @app.post("/strategies")
 def createStrategies(strategy : Strategy):
+    # validation to check that title should not be empty
     if not strategy.title:
         raise HTTPException(status_code=400, detail = "Title cannot be empty")
-    strategy_with_id = strategy.copy(update={"id": uuid4()})
-    strategies.append(strategy_with_id)
+    strategy_with_id = strategy.copy(update={"id": uuid4()})            # unique ID for strategy
+    strategies.append(strategy_with_id)             # adding strategy to in-memory list
     return strategy_with_id
